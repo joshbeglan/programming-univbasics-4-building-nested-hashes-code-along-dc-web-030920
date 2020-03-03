0c416d87a144090f1cbdceb11e30f6ef2a324f81
Josh Beglan <josh_beglan@yahoo.com>
2020-03-03 15:32:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,37 +4,15 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => [
-         {
-           name: "Benvolio",
-           age: "17",
-           attitude: "worried"
-         },
-         {
-           name: "Mercutio",
-           age: "18",
-           attitude: "hot-headed"
-         }
-       ]
+      :hero_friends => []
     },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => [
-         {
-           name: "Steven",
-           age: "30",
-           attitude: "confused"
-         },
-         {
-           name: "Nurse",
-           age: "44",
-           attitude: "worried"
-         }
-       ]
-      }
+      :heroine_friends => []
     }
+  }
 
   
 end
