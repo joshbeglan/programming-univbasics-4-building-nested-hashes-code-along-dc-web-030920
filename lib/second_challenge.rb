f7c775d6b36ba21891099fe3e577192f0ee60406
Josh Beglan <josh_beglan@yahoo.com>
2020-03-03 15:24:10 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,20 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {
-         name: "Lord Montague",
-         age: "53"
-       },
-       :matriarch => {
-         name: "Lady Montague",
-         age: "54"
-       },
-       :hero => {
-         name: "Romeo",
-         age: "15",
-         status: "alive"
-       },
-       :hero_friends => [
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => [
          {
            name: "Benvolio",
            age: "17",
@@ -29,20 +19,10 @@ def second_challenge
      },
       }, 
     :capulet => {
-         :patriarch => {
-         name: "Lord Capulet",
-         age: "50"
-       },
-       :matriarch => {
-         name => "Lady Capulet",
-         age => "51"
-       },
-       :heroine => {
-         name: "Juliet",
-         age: "15",
-         status: "alive"
-       },
-       :heroine_friends => [
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => [
          {
            name: "Steven",
            age: "30",
